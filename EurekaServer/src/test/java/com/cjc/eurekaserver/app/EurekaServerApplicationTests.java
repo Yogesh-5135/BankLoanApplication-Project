c8451db2f027847add1a5adb290d5efd0a567fca
yogesh5135 <yogesh5135@gmail.com>
2024-10-23 15:35:34 +0530

    eureka

---
+++ EurekaServer/src/test/java/com/cjc/eurekaserver/app/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.eurekaserver.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
