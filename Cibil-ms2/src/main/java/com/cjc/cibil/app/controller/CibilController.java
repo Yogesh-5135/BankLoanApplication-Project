abe39ecf451a11b345e3d42102fd4a3648ff0305
omkar3720 <omkarshinde2728@gmail.com>
2024-10-24 13:50:16 +0530

    add

--- Cibil-ms2/src/main/java/com/cjc/cibil/app/controller/CibilController.java
+++ Cibil-ms2/src/main/java/com/cjc/cibil/app/controller/CibilController.java
@@ -3,11 +3,13 @@ package com.cjc.cibil.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cjc.cibil.app.model.Cibil;
 import com.cjc.cibil.app.model.Enquiry;
@@ -21,14 +23,27 @@ public class CibilController {
 	@Autowired
 	CibilServiceI csi;
 	
+	@Autowired
+	RestTemplate rt;
 	
 	@PutMapping("/editCibil/{customerid}")
 	public ResponseEntity<Enquiry> addCibil( @PathVariable int customerid , @RequestBody Cibil c)
 	{
-		Enquiry e = csi.getCustomer(customerid , c);	
 		
+       String url = "http://localhost:9098/random/checkCibilScore";
+		
+		Integer i = rt.getForObject(url, Integer.class);
+		
+		Enquiry e = csi.getCustomer(customerid , c,i);
 		return new ResponseEntity<Enquiry> (e,HttpStatus.OK);
 	}
 	
+	@GetMapping("/checkCibilScore")
+	public void getCibilScore()
+	{
+		
+		
+		//return ne ;
+	}
 	
 }
--- CibilRandom-ms3/src/main/java/com/cjc/cibilrandom/app/controller/RandomController.java
+++ CibilRandom-ms3/src/main/java/com/cjc/cibilrandom/app/controller/RandomController.java
@@ -1,10 +1,23 @@
 package com.cjc.cibilrandom.app.controller;
 
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/random")
-public class RandomController {
+public class RandomController 
+{
+	private final static Random r = new Random();
 
+   @GetMapping("/checkCibilScore")
+   public int create()
+   {
+	   int i = r.nextInt(300,900);
+	   System.out.println(i);  
+	return i;
+	   
+   }
 }
