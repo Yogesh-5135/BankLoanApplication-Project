f0092f0d35959c6ad93311e453185440746826dc
yogesh5135 <yogesh5135@gmail.com>
2024-10-28 08:08:05 +0530

    done

--- Cibil-ms2/src/main/java/com/cjc/cibil/app/serviceimpl/CibilServiceImpl.java
+++ Cibil-ms2/src/main/java/com/cjc/cibil/app/serviceimpl/CibilServiceImpl.java
@@ -19,92 +19,80 @@ import com.cjc.cibil.app.repoi.CibilRepoI;
 import com.cjc.cibil.app.repoi.EnquiryRepoI;
 import com.cjc.cibil.app.servicei.CibilServiceI;
 
-
 @Service
-public class CibilServiceImpl implements CibilServiceI{
+public class CibilServiceImpl implements CibilServiceI {
 
 	@Autowired
 	CibilRepoI cri;
-	
+
 	@Autowired
 	EnquiryRepoI eri;
-	
+
 	@Autowired
 	JavaMailSender jms;
-	
+
 	@Value("${spring.mail.username}")
 	public static String FROM_MAIL;
-	
-	
+
 	private static final Logger log = LoggerFactory.getLogger(CibilServiceImpl.class);
 
 	@Override
-	public Enquiry getCustomer(int customerid ,Integer i ) 
-	{
+	public Enquiry getCustomer(int customerid, Integer i) {
 		LocalDate localDate = LocalDate.now();
 		Date d = Date.valueOf(localDate);
-		
-		Optional<Enquiry> o = eri.findById(customerid);
-		if(o.isPresent()) {
-			Enquiry e = o.get();
-			
-			Cibil c = new Cibil();
-			
-			c.setDate(d);
-			
-			c.setCibilscore(i);	
 
-			if (i >= 750) 
-			{
-			    c.setStatus("Excellent");
-			    c.setRemark("You will get loan as much as you want,Bank is Yours");	    
-			    e.setEnquiryStatus("Approved");
-			} 
-			else if (i >= 650)
-			{
-			    c.setStatus("Good");
-			    c.setRemark("Good score, You will get Loan");
-			    e.setEnquiryStatus("Approved");
-			} 
-			else if (i >= 550) 
-			{
-			    c.setStatus("Average");
-			    c.setRemark("Score is little less, try next time");
-			    e.setEnquiryStatus("Rejected");
+		Optional<Enquiry> o = eri.findById(customerid);
+		if (o.isPresent()) {
+			Enquiry e = o.get();
+
+			Cibil c = new Cibil();
+
+			c.setDate(d);
+
+			c.setCibilscore(i);
+
+			if (i >= 750) {
+				c.setStatus("Excellent");
+				c.setRemark("You will get loan as much as you want,Bank is Yours");
+				e.setEnquiryStatus("Approved");
+			} else if (i >= 650) {
+				c.setStatus("Good");
+				c.setRemark("Good score, You will get Loan");
+				e.setEnquiryStatus("Approved");
+			} else if (i >= 550) {
+				c.setStatus("Average");
+				c.setRemark("Score is little less, try next time");
+				e.setEnquiryStatus("Rejected");
+			} else if (i >= 300) {
+				c.setStatus("Poor");
+				c.setRemark("Better luck next time ");
+				e.setEnquiryStatus("Rejected");
+			} else {
+				c.setStatus("Invalid Score");
+				c.setRemark("You are Great, work hard and improve score");
+				e.setEnquiryStatus("Rejected");
 			}
-			else if (i >= 300) 
-			{
-			    c.setStatus("Poor");
-			    c.setRemark("Better luck next time ");
-			    e.setEnquiryStatus("Rejected");
-			} 
-			else 
-			{
-			   c.setStatus("Invalid Score");
-			    c.setRemark("You are Great, work hard and improve score");
-			    e.setEnquiryStatus("Rejected");
-			}
-			
-			e.setCibil(c);			
+
+			e.setCibil(c);
 			eri.save(e);
-			
+
 			String customeremail = e.getEmail();
 			String enquirystatus = e.getEnquiryStatus();
 			String remark = e.getCibil().getRemark();
+		
+			if(e.getEnquiryStatus().equals("Approved")) {
+				
+				SimpleMailMessage sm = new SimpleMailMessage();
+				sm.setTo(customeremail);
+				sm.setFrom(FROM_MAIL);
+				sm.setSubject("Your Loan is Approved !!");
+				sm.setText("Your Loan status is: " + enquirystatus + "\n" + remark);
+				
+				jms.send(sm);
+			}
 			
-			SimpleMailMessage message = new SimpleMailMessage();
-			message.setFrom(FROM_MAIL);
-			message.setTo(customeremail);
-			message.setSubject("Your Enquiry Status");
-		    message.setText("Your enquiry status is: " + enquirystatus + "\n" + remark);
-		    jms.send(message);
-			
-		    
-		    
 			return e;
-		}
-		else
-		{
+		} else {
 			log.error("Please Enter Valid Customer Id");
 			throw new CustomerNotFoundException("Id not found");
 		}
