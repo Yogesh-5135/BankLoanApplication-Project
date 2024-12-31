7318717028629cb3e9f01f87d68fd4db8d079457
omkar3720 <omkarshinde2728@gmail.com>
2024-12-31 16:00:25 +0530

    add

--- SanctionLetter-CM-ms4/src/main/java/com/cjc/sanctionletter/app/config/Config.java
+++ SanctionLetter-CM-ms4/src/main/java/com/cjc/sanctionletter/app/config/Config.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Config implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) 
+    {
        
         registry.addMapping("/**")    
                 .allowedOrigins("http://localhost:3000")  
